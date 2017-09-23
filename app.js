e653af9a1df463afe7fcd54272ef5fb0e7db7abc
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-23 11:17:04 -0700

    use pug view engine

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const cons = require("consolidate");
-const dust = require("dustjs-helpers");
 const pg = require("pg");
 
 //
 const connectStr = "postgres://webuser:123abc@localhost/recipebook";
 app = express();
-// Assign Dust Engine to .dust files
-app.engine("dust", cons.dust);
-// Set default ext .dust
-app.set("view engine", "dust");
-app.set("views", __dirname + "/views");
+// Set view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 // Set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
